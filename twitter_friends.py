e597edf96fac0f0dc139d5bd52a10f593df3226f
Tsalyk <zalukm1@gmail.com>
2021-02-20 16:40:11 +0200

    increase amount of shown friends

--- twitter_friends.py
+++ twitter_friends.py
@@ -18,7 +18,7 @@ def parse_data(nickname: str) -> dict:
 
     search_params = {
         "screen_name": nickname,
-        "count": 10
+        "count": 100
     }
 
     response = requests.get(search_url,
